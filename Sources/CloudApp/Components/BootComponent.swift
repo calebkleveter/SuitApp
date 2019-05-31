ba196be3c00ccd21a2282640a52292ba4b7f57a9
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-31 12:23:05 -0500

    Fade BootComponent message in and out

--- Sources/CloudApp/Components/BootComponent.swift
+++ Sources/CloudApp/Components/BootComponent.swift
@@ -10,8 +10,21 @@ final class BootComponent: CompositeComponent {
         introLabel.width = 100%
         introLabel.height = 30~
         introLabel.horizontalArrangement = .center
+        introLabel.textColor = Color(red: 38/255, green: 38/255, blue: 38/255)
+
+        self.animate(label: introLabel, property: \.textColor, to: Color(red: 38/255, green: 38/255, blue: 38/255, alpha: 0))
 
         self.view.add(subview: introLabel)
         self.view.justifyContent = .center
+    }
+
+    func animate<T>(label: Label, property: ReferenceWritableKeyPath<Label, T>, to value: T, duration: Double = 1.0) {
+        let originalValue = label[keyPath: property]
+
+        label.animate(duration: duration, delay: 0, easing: .linear, changes: {
+            label[keyPath: property] = value
+        }, completion: {
+            self.animate(label: label, property: property, to: originalValue, duration: duration)
+        })
     }
 }

cb9894a8390c3ed4015514476188a40e407aa16c
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-31 15:20:59 -0500

    Changed window title bar text to 'My App'

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,6 +2,6 @@ import Foundation
 import Suit
 
 let mainFrame = CGRect(x: 0, y: 0, width: 800, height: 600)
-let main = Window(rootComponent: BootComponent(), frame: mainFrame, hasTitleBar: true, title: "Vapor Cloud")
+let main = Window(rootComponent: BootComponent(), frame: mainFrame, hasTitleBar: true, title: "My App")
 
 Application.create(with: main).launch()
